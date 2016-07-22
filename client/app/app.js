ee8dcdbc39c34c8525c9b909d0ea5d04dbf0239d
Mike Frazier <michael.s.frazier@gmail.com>
2016-07-22 12:53:22 -0500

    Create app and config initial routing

---
+++ client/app/app.js
@@ -0,0 +1,17 @@
+angular.module("weather", [
+  'ngRoute'
+  ])
+.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl : 'app/main/main.html',
+      controller: 'app/main/MainController'
+    })
+    .when('/forecast', {
+      template: 'app/forecast/forecast.html',
+      controller: 'app/forecast/ForecastController'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
